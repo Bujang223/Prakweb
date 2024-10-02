d4089559c16fcb7416b92f1bcbfa831f2d7f88f6
Bujang223 <144742956+Bujang223@users.noreply.github.com>
2024-10-02 13:54:12 +0700

    Create isi.js

---
+++ isi.js
@@ -0,0 +1,8 @@
+document.writeln("<center>");
+document.writeln("<hr width=600 color=Black>");
+document.writeln("<h1>UNIVERSITAS LAMPUNG</h1>");
+document.writeln("<h1>PENDIDIKAN TEKNOLOGI INFORMASI</h1>");
+document.writeln("<h1>JURUSAN PENDIDIKAN MATEMATIKA DAN ILMU PENGETAHUAN ALAM</h1>");
+document.writeln("<h1>UNIVERSITAS LAMPUNG</h1>");
+document.writeln("<hr width=600 color=Black>");
+document.writeln("</center>");
